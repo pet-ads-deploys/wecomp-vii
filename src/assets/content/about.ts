926972cdcaa3a2673cc165785e536dfbce2ffa14
Mariana Sônego <sonegomariana22@gmail.com>
2026-09-16 11:38:44 -0300

    fix: correct typo in description

--- src/assets/content/about.ts
+++ src/assets/content/about.ts
@@ -6,7 +6,7 @@ interface AboutProps {
 export const aboutContent: AboutProps = {
   title: "Sobre a Wecomp",
   description:
-    "A Wecomp é a semana da computação do IFSP São Carlos, que neste ano sua sétima edição. O objetivo do evento é proporcionar uma maior interação entre os estudantes, o mercado de tecnologia e a cidade de São Carlos. Ao longo de cinco dias de evento, serão realizados diversos minicursos, talks e processos seletivos, que visam reduzir a distância entre a teoria e a prática, ensinar tecnologias novas e promover o ingresso dos participantes no mercado de trabalho.",
+    "A Wecomp é a Semana da Computação do IFSP São Carlos, que chega este ano à sua sétima edição. O objetivo do evento é aproximar os estudantes, o mercado de tecnologia e a comunidade local. Ao longo de cinco dias, serão realizados diversos minicursos, palestras e processos seletivos voltados a integrar teoria e prática, ensinar novas tecnologias e impulsionar o ingresso dos participantes no mercado de trabalho.",
 };
 
 export default aboutContent;
